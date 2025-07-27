1192bbee92acc81bb9db2a98db6ce59f7660d532
supra <alexusenzoroa@gmail.com>
2025-07-28 03:21:19 +0800

    It is finally perfect now! every time pacman changes direction, his position gets readjusted, this keeps him centered at all times in the game! GOD DAMN HOLY SHIT I WONDER WHAT OTHER BULLSHIT I'M GONNA FACE APSJDOIJGD:OIJRG:OINERO:GNERO

--- src/main.rs
+++ src/main.rs
@@ -14,94 +14,72 @@ async fn main() {
     let map_image: Texture2D = load_texture("assets/pacmaze2.png").await.unwrap();
     let mut game_map = RAW_MAP;
     let mut input_buffer = vec2(0.0, 0.0);
+    let frame_time = 1.0 / FPS;
     let mut pacman = PacMan {
         position: vec2(CENTER.x, CENTER.y + 256.0),
         size: TILE_SIZE,
         direction: vec2(0.0, 0.0),
-        speed: 240.0,
+        speed: 300.0,
         powered_up: false,
     };
     let mut pacman_collision_checker = PacMan {
         position: vec2(CENTER.x, CENTER.y + 256.0),
         size: TILE_SIZE / 4.0,
         direction: vec2(0.0, 0.0),
-        speed: 240.0,
+        speed: 300.0,
         powered_up: false,
     };
     loop {
-        let frame_time = 1.0 / FPS;
-
         draw_elements(game_map, &map_image);
         let row = ((pacman_collision_checker.position.x - BOARD_TOP_LEFT_COORDS.x) / TILE_SIZE)
             .floor() as usize;
         let col = ((pacman_collision_checker.position.y - BOARD_TOP_LEFT_COORDS.y) / TILE_SIZE)
             .floor() as usize;
-
-        pacman_collision_checker.position +=
-            pacman_collision_checker.direction * pacman_collision_checker.speed * frame_time;
-
         if let Some(direction) = handle_controls() {
             input_buffer = direction;
         }
 
+        // note, one thing I noticed is that, move confirmation must be done before this
+        // line: pacman_collision_checker.position += ....
+        // let pacman_collision_pos = (
+        //     pacman_collision_checker.position.x,
+        //     pacman_collision_checker.position.y,
+        // );
+
+        pacman_collision_checker.position +=
+            pacman_collision_checker.direction * pacman_collision_checker.speed * frame_time;
+
         let mut colliding = false;
         let walls = load_walls(game_map);
-        // MAIN COLLISION CHECKING
-        if pacman_collision_checker.direction == vec2(1.0, 0.0) {
-            if collision_check(
-                vec2(
-                    pacman_collision_checker.position.x + 12.0,
-                    pacman_collision_checker.position.y,
-                ),
-                pacman_collision_checker.size,
-                walls,
-            ) {
-                colliding = true;
+        if collision_checking_offset(&pacman_collision_checker, &walls) {
+            colliding = true;
+        }
+
+        if pacman_collision_checker.direction != input_buffer {
+            if can_move_to_direction(row, col, input_buffer) {
+                pacman_collision_checker.position =
+                    centered_coordinates(pacman_collision_checker.position);
+                pacman_collision_checker.direction = input_buffer;
             }
-        } else if pacman_collision_checker.direction == vec2(-1.0, 0.0) {
-            if collision_check(
-                vec2(
-                    pacman_collision_checker.position.x - 12.0,
-                    pacman_collision_checker.position.y,
-                ),
-                pacman_collision_checker.size,
-                walls,
-            ) {
-                colliding = true;
-            }
-        } else if pacman_collision_checker.direction == vec2(0.0, 1.0) {
-            if collision_check(
-                vec2(
-                    pacman_collision_checker.position.x,
-                    pacman_collision_checker.position.y + 12.0,
-                ),
-                pacman_collision_checker.size,
-                walls,
-            ) {
-                colliding = true;
-            }
-        } else if pacman_collision_checker.direction == vec2(0.0, -1.0) {
-            if collision_check(
-                vec2(
-                    pacman_collision_checker.position.x,
-                    pacman_collision_checker.position.y - 12.0,
-                ),
-                pacman_collision_checker.size,
-                walls,
-            ) {
-                colliding = true;
-            }
+        } else {
+            pacman_collision_checker.direction = input_buffer;
         }
 
         if colliding {
             pacman_collision_checker.position.x = pacman.position.x;
             pacman_collision_checker.position.y = pacman.position.y;
         }
-        if can_move_to_direction(col, row, input_buffer) {
-            pacman_collision_checker.direction = input_buffer;
+
+        if pacman_collision_checker.position.x > 1028.0 {
+            pacman_collision_checker.position.x = 204.0;
+        } else if pacman_collision_checker.position.x < 204.0 {
+            pacman_collision_checker.position.x = 1028.0;
         }
 
-        pacman.position = pacman_collision_checker.position;
+        pacman.position = vec2(
+            (pacman_collision_checker.position.x).floor(),
+            (pacman_collision_checker.position.y).floor(),
+        );
         draw_characters(&pacman);
         debug_texts(&pacman_collision_checker, row, col, colliding);
         game_map = pacman_food_eat(game_map, row, col);
--- src/map_operations.rs
+++ src/map_operations.rs
@@ -99,8 +99,8 @@ pub fn aabb_collision(center_a: Vec2, size_a: f32, center_b: Vec2, size_b: f32) 
     min_a.x < max_b.x && max_a.x > min_b.x && min_a.y < max_b.y && max_a.y > min_b.y
 }
 
-pub fn collision_check(pacman_pos: Vec2, pacman_size: f32, walls: Vec<Wall>) -> bool {
-    for wall in &walls {
+pub fn collision_check(pacman_pos: Vec2, pacman_size: f32, walls: &Vec<Wall>) -> bool {
+    for wall in walls {
         if aabb_collision(
             pacman_pos,
             pacman_size,
@@ -111,6 +111,44 @@ pub fn collision_check(pacman_pos: Vec2, pacman_size: f32, walls: Vec<Wall>) -> 
         }
     }
     false
+}
+
+pub fn collision_checking_offset(pacman: &PacMan, walls: &Vec<Wall>) -> bool {
+    if pacman.direction == vec2(1.0, 0.0)
+        && collision_check(
+            vec2(pacman.position.x + 12.0, pacman.position.y),
+            pacman.size,
+            walls,
+        )
+    {
+        true
+    } else if pacman.direction == vec2(-1.0, 0.0)
+        && collision_check(
+            vec2(pacman.position.x - 12.0, pacman.position.y),
+            pacman.size,
+            walls,
+        )
+    {
+        true
+    } else if pacman.direction == vec2(0.0, 1.0)
+        && collision_check(
+            vec2(pacman.position.x, pacman.position.y + 12.0),
+            pacman.size,
+            walls,
+        )
+    {
+        true
+    } else if pacman.direction == vec2(0.0, -1.0)
+        && collision_check(
+            vec2(pacman.position.x, pacman.position.y - 12.0),
+            pacman.size,
+            walls,
+        )
+    {
+        true
+    } else {
+        false
+    }
 }
 
 pub fn handle_controls() -> Option<Vec2> {
@@ -129,44 +167,36 @@ pub fn handle_controls() -> Option<Vec2> {
     }
 }
 
-pub fn pacman_food_eat(mut map: [[u8; COLS]; ROWS], row: usize, col: usize) -> [[u8; COLS]; ROWS] {
-    if map[col][row] == 2 || map[col][row] == 3 {
-        map[col][row] = 0;
+pub fn pacman_food_eat(mut map: [[u8; COLS]; ROWS], col: usize, row: usize) -> [[u8; COLS]; ROWS] {
+    if map[row][col] == 2 || map[row][col] == 3 {
+        map[row][col] = 0;
     }
     return map;
 }
 pub fn can_move_to_direction(col: usize, row: usize, direction: Vec2) -> bool {
     // RAW_MAP[y][x] != 1
-    if direction == vec2(1.0, 0.0) && RAW_MAP[col][row + 1] == 1 {
+    // this must be changed here to adjust pacman's collision checker a few pixels behind
+    // where he is going, probably 16 pixels to make input buffering confirmation more "aligned"
+    // let row = ((pacman.position.x - BOARD_TOP_LEFT_COORDS.x) / TILE_SIZE).floor() as usize;
+    // let col = ((pacman.position.y - BOARD_TOP_LEFT_COORDS.y) / TILE_SIZE).floor() as usize;
+    //
+    if direction == vec2(1.0, 0.0) && RAW_MAP[row][col + 1] == 1 {
         false
-    } else if direction == vec2(-1.0, 0.0) && RAW_MAP[col][row - 1] == 1 {
+    } else if direction == vec2(-1.0, 0.0) && RAW_MAP[row][col - 1] == 1 {
         false
-    } else if direction == vec2(0.0, 1.0) && RAW_MAP[col + 1][row] == 1 {
+    } else if direction == vec2(0.0, 1.0) && RAW_MAP[row + 1][col] == 1 {
         false
-    } else if direction == vec2(0.0, -1.0) && RAW_MAP[col - 1][row] == 1 {
+    } else if direction == vec2(0.0, -1.0) && RAW_MAP[row - 1][col] == 1 {
         false
     } else {
         true
     }
 }
-pub fn is_x_aligned(pacman: &PacMan) -> bool {
-    pacman.position.x % 16.0 == 0.0
-}
 
-pub fn align_x(pos_x: f32, direction: Vec2) -> f32 {
-    let aligned_x: f32;
-    if direction == vec2(1.0, 0.0) {
-        aligned_x = pos_x - 16.0;
-        aligned_x
-    } else {
-        pos_x
-    }
-}
-
-pub fn debug_texts(pacman: &PacMan, row: usize, col: usize, colliding: bool) {
+pub fn debug_texts(pacman: &PacMan, col: usize, row: usize, colliding: bool) {
     let pacman_pos_string = &pacman.position.to_string();
-    let row_string = &col.to_string();
-    let col_string = &row.to_string();
+    let col_string = &col.to_string();
+    let row_string = &row.to_string();
 
     if colliding {
         let collision_text = "COLLIDING";
@@ -174,6 +204,27 @@ pub fn debug_texts(pacman: &PacMan, row: usize, col: usize, colliding: bool) {
     }
 
     draw_text(pacman_pos_string, 50.0, 35.0, 15.0, YELLOW);
-    draw_text(row_string, 50.0, 50.0, 15.0, YELLOW);
-    draw_text(col_string, 50.0, 65.0, 15.0, YELLOW);
+    draw_text(col_string, 50.0, 50.0, 15.0, YELLOW);
+    draw_text(row_string, 50.0, 65.0, 15.0, YELLOW);
+}
+
+pub fn is_aligned_x(pacman_pos: f32) -> bool {
+    if pacman_pos % 16.0 == 0.0 {
+        true
+    } else {
+        false
+    }
+}
+
+pub fn centered_coordinates(pacman_pos: Vec2) -> Vec2 {
+    let row = ((pacman_pos.x - BOARD_TOP_LEFT_COORDS.x) / TILE_SIZE).floor() as f32;
+    let col = ((pacman_pos.y - BOARD_TOP_LEFT_COORDS.y) / TILE_SIZE).floor() as f32;
+    //
+    // let col = (pacman_pos.x / tile_size).floor() as i32;
+    // let row = (pacman_pos.y / tile_size).floor() as i32;
+    // let centered_x = (pacman_pos.x / tile_size).round() * tile_size;
+    // let centered_y = (pacman_pos.y / tile_size).round() * tile_size;
+    let centered_x = ((row * TILE_SIZE) + BOARD_TOP_LEFT_COORDS.x) + 16.0;
+    let centered_y = ((col * TILE_SIZE) + BOARD_TOP_LEFT_COORDS.y) + 16.0;
+    vec2(centered_x, centered_y)
 }
